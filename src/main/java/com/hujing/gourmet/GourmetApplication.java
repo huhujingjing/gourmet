3ec4c476180724063a68445f1ebbaab6cf5ebaa0
hujing <15372026611@163.com>
2019-01-23 18:01:39 +0800

    1-添加各种包以及实体类、控制器等等

--- src/main/java/com/hujing/gourmet/GourmetApplication.java
+++ src/main/java/com/hujing/gourmet/GourmetApplication.java
@@ -2,7 +2,13 @@ package com.hujing.gourmet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 
+//扫描neo4j库操作包路径，一定要有而且要配置正确
+@EnableNeo4jRepositories(basePackages = {"com.hujing.repository." })
+//扫描neo4j实体类包路径，重申一定要有而且要配置正确
+@EntityScan(basePackages = {"com.hujing.entity."})
 @SpringBootApplication
 public class GourmetApplication {
 
